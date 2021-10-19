21191513c99b3e8e6c1a22a2d7ea491b45316b16
sakawatchy434 <hossainshakawat434@gmail.com>
2021-10-19 11:40:53 +0600

    first commit

--- src/pages/Share/Header/Header.js
+++ src/pages/Share/Header/Header.js
@@ -10,16 +10,21 @@ const Header = () => {
             <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
                 <Container>
                     <Navbar.Brand href="#home"><GiHospital className="me-2" /> Central Hospital</Navbar.Brand>
-                    <Nav className="ms-auto nav-link">
-                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#servics">Services</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#doctors">Doctors</Nav.Link>
-                        <Nav.Link as={HashLink} to="/contact">Contact</Nav.Link>
-                        <Nav.Link as={HashLink} to="/login">Login</Nav.Link>
-                    </Nav>
-                    <Navbar.Text>
-                        Signed in as: <a href="#login">Mark Otto</a>
-                    </Navbar.Text>
+
+
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav className="ms-auto nav-link">
+                            <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#servics">Services</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#doctors">Doctors</Nav.Link>
+                            <Nav.Link as={HashLink} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={HashLink} to="/login">Login</Nav.Link>
+                        </Nav>
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">Mark Otto</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
             <br />
